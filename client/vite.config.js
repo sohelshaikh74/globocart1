8c0b90860f54d0038e9a0c34e2fff524251838ea
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-05-03 13:30:40 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist", // Explicitly tell Vite where to output files
+    outDir: "dist", // Ensure Vite outputs to the 'dist' directory
+  },
+  server: {
+    historyApiFallback: true, // Important for client-side routing
   },
 });
